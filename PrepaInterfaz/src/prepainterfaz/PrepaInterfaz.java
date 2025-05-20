e7bae3b1eb59bbfe47cbed446371c52da534c940
Antony Cen <antony.cen@correo.unimet.edu.ve>
2025-05-19 23:21:23 -0400

    Se agrego PrepaInterfaz

---
+++ PrepaInterfaz/src/prepainterfaz/PrepaInterfaz.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prepainterfaz;
+
+/**
+ *
+ * @author anton
+ */
+public class PrepaInterfaz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Ventana interfaz=new Ventana();
+        interfaz.show();
+        interfaz.setLocationRelativeTo(null);
+    }
+    
+}
